0701a3b6e911cf42fd395777c5e84ac7441e6dd6
marisasha <marisasha228@bk.ru>
2026-03-04 01:25:10 +0300

    combine migrations

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,1 +1,3 @@
+DROP TABLE email_notification;
+DROP TABLE email_verification_tokens;
 DROP TABLE users;
--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -4,7 +4,26 @@ CREATE TABLE users (
     password_hash VARCHAR(255) NOT NULL,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
-    registered_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    registered_at DATETIME DEFAULT NOW(),
     email_verified TINYINT(1) DEFAULT 0
 ) ENGINE=InnoDB;
 
+CREATE TABLE email_verification_tokens (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    token VARCHAR(255) NOT NULL UNIQUE,
+    created_at DATETIME DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE email_notification(
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    text TEXT NOT NULL,
+    send_at DATETIME NOT NULL,
+    status ENUM('pending','sent', 'failed') DEFAULT 'pending',
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+
+    INDEX idx_status_send_at (status, send_at)
+) ENGINE=InnoDB;
